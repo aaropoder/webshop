d01182f60e80d291f2ee16d3078a5fc22b95c355
Põder Aaro <aarpo@teklaad.tekla.com>
2021-03-16 12:18:32 +0200

    translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,8 +35,19 @@ import { ViewItemsComponent } from './admin/view-items/view-items.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
--- src/app/global/navbar/navbar.component.ts
+++ src/app/global/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { CartService } from 'src/app/cart/cart.service';
 export class NavbarComponent implements OnInit {
   cartSum: number = 0;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private translate: TranslateService) {}
 
   // arrow function (items) => on justkui sama function(items) ehk ES6 ja ES5
 
@@ -20,5 +21,15 @@ export class NavbarComponent implements OnInit {
         this.cartSum += item.price;
       });
     });
+    let lang = localStorage.getItem('language');
+    // if (lang != null) {
+    if (lang) {
+      this.useLanguage(lang);
+    }
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+    localStorage.setItem('language', language);
   }
 }
